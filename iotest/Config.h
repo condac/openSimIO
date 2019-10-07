5f53f2a335181ed774432bf5ee959af4b469e3ac
condac <burns.nu@gmail.com>
2019-10-07 12:37:01 +0200

    Added basic LCD support in arduino program

--- iotest/Config.h
+++ iotest/Config.h
@@ -60,7 +60,7 @@ IPAddress ip(192, 168, 0, 105);
 // LCD
 // NOTE! You need to configure LCD in lcd.h
 // There are just to many options to do this through the plugin
-#define LCD
+//#define LCD
 
 
 
